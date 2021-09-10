d4384bb33b16cedaee2f983072865c52a35e9496
Victoria Carr <vc11@sanger.ac.uk>
2021-09-10 08:47:19 +0100

    Remove read names

--- bin/collect_IS_annotations.R
+++ bin/collect_IS_annotations.R
@@ -127,9 +127,7 @@ itrs_summary <- itrs %>%
 
 # Get read names
 reads_with_itr_cluster <- itrs[itrs$itr_cluster %in% unlist(strsplit(itrs_summary$itr_clusters, ";")),] %>% select(itr_cluster, read1, read2)
-ir_read_names <- c(reads_with_itr_cluster$read1, reads_with_itr_cluster$read2)
 
 ### Write output
 write.table(itrs_summary, paste0(opt$output, "_insertion_sequence_annotations.tab"), row.names = FALSE, quote = FALSE, sep = "\t")
-write.table(ir_read_names, paste0(opt$output, "_read_names.txt"), row.names = FALSE, quote = FALSE, sep = "\t", col.names = FALSE)
 write.table(reads_with_itr_cluster, paste0(opt$output, "_reads_itr_clusters.txt"), row.names = FALSE, quote = FALSE, sep = "\t")
